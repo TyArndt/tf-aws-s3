bdf4ec1cc06fc2db7b71b46171fe82849cab8a1f
Tyler-Arndt <tyler.arndt@associatedbank.com>
2023-11-10 23:50:38 -0600

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "this" {
+    bucket = var.bucket_name
+    tags = module.tags
+}
+
+module "tags" {
+    source = "git::https://github.com/TyArndt/tf-aws-tags.git"
+    baseline_tags = locals.baseline_tags
+}
+
+variable bucket_name {
+    type = string
+}
+
+variable "baseline_tags" {
+  type = map(string)
+}
